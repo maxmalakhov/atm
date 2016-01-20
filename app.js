38d9e0058f56c618deff28162bb30f5aaaedb8a8
Max Malakhov <malakhovbox@gmail.com>
2016-01-20 13:50:44 +0100

    #ATM-3 Configuration and cleanup

--- app.js
+++ app.js
@@ -5,14 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var cards = require('./routes/cards');
+//var deposits = require('./routes/deposits');
+//var withdraw = require('./routes/withdraw');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(favicon());
 app.use(logger('dev'));
@@ -21,8 +18,9 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//app.use('/cards', cards);
+//app.use('/deposits', deposits);
+//app.use('/withdraw', withdraw);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -32,28 +30,22 @@ app.use(function(req, res, next) {
 });
 
 /// error handlers
-
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
-        res.render('error', {
+        res.json({
             message: err.message,
             error: err
         });
     });
 }
 
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.json({
         message: err.message,
-        error: {}
+        error: err
     });
 });
-
 
 module.exports = app;
